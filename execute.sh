0cb63bf16df4d84a1b00f3fb5e2aa485e78b854b
QiyuHu <422207334@qq.com>
2023-11-02 19:24:35 +0800

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -10,7 +10,6 @@ TF_ENABLE_ONEDNN_OPTS=0 CUDA_VISIBLE_DEVICES=0  python3  run_class_main.py \
     --save_freq 2   \
     --mask_ratio 0.0 \
     --train_type std_train \
-    --resume /Data1/zhangguangyi/SemanRes1/PaperCode_V2/ckpt_record/ckpt_std_train/checkpoint-65.pth \
     --if_attack_train  \
     --if_attack_test \
     --eval
